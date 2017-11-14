ee52f9c340ce7abbfd533fb9d5d17d90d9feb50a
ruanyto66 <ruan_c3@hotmail.com>
2017-11-14 21:00:59 +0000

    adicionando crud para teatro

--- src/Handler/Teatro.hs
+++ src/Handler/Teatro.hs
@@ -17,19 +17,51 @@ postTeatroR = do
     teatroid <- runDB $ insert teatro
     sendStatusJSON created201 (object ["resp" .= (fromSqlKey teatroid)])
     
+
 getConsultaTeatroR :: TeatroId -> Handler Value
 getConsultaTeatroR teatroid = do
     teatro <- runDB $ get404 teatroid
     sendStatusJSON ok200 (object ["resp" .= toJSON teatro])
-
+    
 getTodosTeatrosR :: Handler Value
 getTodosTeatrosR = do
     teatros <- runDB $ selectList [] [Asc TeatroNome]
     sendStatusJSON ok200 (object ["resp" .= toJSON teatros])
-   
+    
 deleteApagarTeatroR :: TeatroId -> Handler Value
 deleteApagarTeatroR teatroid = do
     _ <- runDB $ get404 teatroid
     runDB $ delete teatroid
     sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
-   
+    
+putAlterarTeatroR :: TeatroId -> Handler Value
+putAlterarTeatroR teatroid = do
+    _ <- runDB $ get404 teatroid
+    novoTeatro <- requireJsonBody :: Handler Teatro
+    runDB $ replace teatroid novoTeatro
+    sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
+    
+patchAlterarTeNomeR :: TeatroId -> Text -> Handler Value
+patchAlterarTeNomeR teatroid nome = do
+    _ <- runDB $ update teatroid [TeatroNome =. nome]
+    sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
+
+patchAlterarTeHorarioR :: TeatroId -> UTCTime -> Handler Value
+patchAlterarTeHorarioR teatroid horario = do
+    _ <- runDB $ update teatroid [TeatroHorario =. horario]
+    sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
+
+patchAlterarTeDescricaoR :: TeatroId -> Text -> Handler Value
+patchAlterarTeDescricaoR teatroid descricao = do
+    _ <-runDB $ update teatroid [TeatroDescricao =. descricao]
+    sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
+    
+patchAlterarTeTelR :: TeatroId -> Int -> Handler Value
+patchAlterarTeTelR teatroid tel = do
+    _ <- runDB $ update teatroid [TeatroTel =. tel]
+    sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
+    
+patchAlterarTeEnderecoR :: TeatroId -> Text -> Handler Value
+patchAlterarTeEnderecoR teatroid endereco = do
+    _ <- runDB $ update teatroid [TeatroEndereco =. endereco]
+    sendStatusJSON noContent204 (object ["resp" .= toJSON teatroid])
